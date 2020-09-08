237ddd98835c3a65815cb198edadf09939b93ed4
Roman Gonyukov <roman@bx24.store>
2020-09-08 12:05:45 +0700

    change partner uri in lang file

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -2,4 +2,4 @@
 
 $MESS['EV_MODULE_NAME'] = 'Детальная страница элемента универсальных списков';
 $MESS['EV_PARTNER_NAME'] = 'Ramapriya Doom';
-$MESS['EV_PARTNER_URI'] = 'https://github.com/stayfuneral';
+$MESS['EV_PARTNER_URI'] = 'https://github.com/stayfuneral/ramapriya.elementviewer';
